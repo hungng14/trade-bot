69ed0ce1c132784d1a84c4f8251f671e9aedfde4
hungng14 <hungng14@gmail.com>
2022-01-05 23:06:37 +0700

    up

--- src/my-bot.js
+++ src/my-bot.js
@@ -2,5 +2,4 @@ const TelegramBot = require('node-telegram-bot-api');
 const { BOT_TELEGRAM_TOKEN } = require('./constants');
 
 const bot = new TelegramBot(BOT_TELEGRAM_TOKEN, { polling: true });
-
 module.exports = bot;
