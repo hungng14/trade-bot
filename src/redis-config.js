06385afec513b1b52e33bf928a4ce9dcaab8bef5
hungng14 <hungng14@gmail.com>
2022-01-05 16:05:59 +0700

    up

--- src/redis-config.js
+++ src/redis-config.js
@@ -1,6 +1,8 @@
 const redis = require("redis");
 const { promisify } = require("util");
-const client = redis.createClient();
+const client = redis.createClient({
+    url: process.env.REDIS_URL,
+});
 client.get = promisify(client.get);
 
 module.exports = client;
